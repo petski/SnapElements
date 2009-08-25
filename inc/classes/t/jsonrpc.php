165f32d82a337b5cbf686ecf8ac5fbadf2d528a7
Ivo van der Meer <ivo@crewtty.ath.cx>
2009-08-25 17:07:52 +0200

    Whoops... fix require_once statement.

--- inc/classes/t/jsonrpc.php
+++ inc/classes/t/jsonrpc.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("../Service.php");
+require_once("../Services.php");
 $jsonrpc = new Service;
 $p = array('name' => 'poef.nl', 'type' => 'NATIVE');
 $jsonrpc->queue_domain_add((object)$p);
