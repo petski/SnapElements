d8819d86103ef57ca8e5a707cc338c24731f5d48
Ivo van der Meer <ivo@crewtty.ath.cx>
2009-08-26 17:12:26 +0800

    Redesign: table queue has a one to many relationship with table queue_item_domains and queue_item_records.
    Signed-off-by: Patrick Kuijvenhoven <pet@painfullscratch.nl>

--- install/db.sql
+++ install/db.sql
@@ -1,5 +1,6 @@
 --
 -- Begin PowerDNS MySQL table structure
+-- PowerDNS structure base on: http://doc.powerdns.com/configuring-db-connection.html#CONFIGURING-MYSQL 
 --
 
 --
@@ -17,10 +18,11 @@ CREATE TABLE `domains` (
   `type` varchar(6) NOT NULL,
   `notified_serial` int(11) DEFAULT NULL,
   `account` varchar(40) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name_index` (`name`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
+
+CREATE UNIQUE INDEX name_index ON domains(name);
 
 --
 -- Table structure for table `records`
@@ -38,12 +40,13 @@ CREATE TABLE `records` (
   `ttl` int(11) DEFAULT NULL,
   `prio` int(11) DEFAULT NULL,
   `change_date` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `rec_name_index` (`name`),
-  KEY `nametype_index` (`name`,`type`),
-  KEY `domain_id` (`domain_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 SET character_set_client = @saved_cs_client;
+
+CREATE INDEX rec_name_index ON records(name);
+CREATE INDEX nametype_index ON records(name,type);
+CREATE INDEX domain_id ON records(domain_id);
 
 --
 -- Table structure for table `supermasters`
@@ -65,7 +68,7 @@ SET character_set_client = @saved_cs_client;
 
 
 --
--- Begin our Custom MySQL table structure
+-- Begin SnapElements MySQL table structure
 --
 
 --
@@ -83,30 +86,58 @@ CREATE TABLE `queue` (
   `archived` tinyint(1) NOT NULL DEFAULT '0',
   `closed` tinyint(1) NOT NULL DEFAULT '0',
   `user_id` int(11) DEFAULT NULL,
-  `user_name` varchar(16) COLLATE latin1_general_ci NOT NULL DEFAULT '',
   `comment` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 SET character_set_client = @saved_cs_client;
 
 --
--- Table structure for table `queue_item`
+-- Table structure for table `queue_item_domains`
 --
 
-DROP TABLE IF EXISTS `queue_items`;
+DROP TABLE IF EXISTS `queue_item_domains`;
 SET @saved_cs_client     = @@character_set_client;
 SET character_set_client = utf8;
-CREATE TABLE `queue_items` (
+CREATE TABLE `queue_item_domains` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ch_date` datetime NOT NULL,
   `queue_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
   `function` varchar(50) COLLATE latin1_general_ci NOT NULL,
-  `change` text COLLATE latin1_general_ci,
+  `name` varchar(255) NOT NULL,
+  `master` varchar(128) DEFAULT NULL,
+  `last_check` int(11) DEFAULT NULL,
+  `type` varchar(6) NOT NULL,
+  `notified_serial` int(11) DEFAULT NULL,
+  `account` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 SET character_set_client = @saved_cs_client;
 
+--
+-- Table structure for table `queue_item_records`
+--
+
+DROP TABLE IF EXISTS `queue_item_records`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `queue_item_records` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ch_date` datetime NOT NULL,
+  `queue_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `function` varchar(50) COLLATE latin1_general_ci NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `type` varchar(6) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `ttl` int(11) DEFAULT NULL,
+  `prio` int(11) DEFAULT NULL,
+  `change_date` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+SET character_set_client = @saved_cs_client;
 
 --
--- End our Custom MySQL table structure
+-- End SnapElements MySQL table structure
 --
 
